bd055c9d1827205e15583533f6868775749faf26
Furkan Soydan <>
2022-08-24 01:51:52 +0300

    second commit

--- GitTestProject/ViewController.swift
+++ GitTestProject/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         print("Git")
         print("Git git")
+        print("Git git git")
     }
 
 
